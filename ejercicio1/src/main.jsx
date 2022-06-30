fd8d37869e5f8f87a28f30db2571fe4d0c90b23e
ttejeda <ttejeda@ucol.mx>
2022-06-29 23:24:16 -0500

    implemented Content (no complete) and Total functions

--- ejercicio1/src/main.jsx
+++ ejercicio1/src/main.jsx
@@ -3,26 +3,22 @@ import ReactDOM from 'react-dom/client'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    'Fundamentals of React',
+    'Using props to pass data',
+    'State of a component'
+  ]
+  const exercises = [
+    10,
+    7,
+    14
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content parts={parts} exercises={exercises}/>
+      <Total exercises={exercises}/>
     </div>
   )
 }
@@ -31,6 +27,28 @@ const Header = ({course}) => {
   return (
     <div>
       <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Content = ({parts}, {exercises}) => {
+  return (
+    <ul>
+      {parts.map((part, index) => (
+        <li>{part}</li>
+      ))}
+    </ul>
+  )
+}
+
+const Total = ({exercises}) => {
+  let temp = 0;
+  exercises.map(exercise => (
+    temp += exercise
+  ))
+  return (
+    <div>
+      <p>Number of exercises {temp}</p>
     </div>
   )
 }
